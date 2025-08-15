f6fa59ef546fcee2dce4d016faae249190734675
Chaim Malov <chaimalov@gmail.com>
2025-08-15 18:48:24 +0300

    refactor: update virtual list rendering logic to use renderedItems and improve data structure

--- src/queue/virtual-list.component.ts
+++ src/queue/virtual-list.component.ts
@@ -15,20 +15,35 @@ import {
   input,
   linkedSignal,
   runInInjectionContext,
-  signal,
   viewChild,
 } from '@angular/core';
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
-import { debounceTime, fromEvent, map, tap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { debounceTime, fromEvent, map } from 'rxjs';
 
 export const Observer = new InjectionToken<IntersectionObserver>('IntersectionObserver');
 export type Observer = IntersectionObserver;
 
 export interface VirtualItem<T> {
   $index: number;
-  item: T;
+  data: T;
 }
 
+/**
+ * A virtual list component that only renders the items that are currently visible in the viewport.
+ *
+ * This component is useful for rendering large lists of items without overwhelming the browser with DOM elements.
+ *
+ * @example
+ * ```html
+ * <app-virtual-list [items]="items" #virtualList>
+ *   `@for (item of virtualList.renderedItems(); track item.data.id) {
+ *     <app-virtual-item>
+ *        <!-- your content here -->
+ *     </app-virtual-item>
+ *   }`
+ * </app-virtual-list>
+ * ```
+ */
 @Component({
   selector: 'app-virtual-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -46,68 +61,16 @@ export interface VirtualItem<T> {
   ],
 })
 export class VirtualListComponent<T> {
-  placeholderHeight = inject(new HostAttributeToken('placeholderHeight'), { optional: true }) ?? '100px';
-  rootMargin = inject(new HostAttributeToken('rootMargin'), { optional: true }) ?? '500px';
+  public items = input.required<T[]>();
 
-  public items = input<T[]>([]);
-
+  private rootMargin = inject(new HostAttributeToken('rootMargin'), { optional: true }) ?? '500px';
   private injector = inject(Injector);
   private destroyRef = inject(DestroyRef);
   private element = inject<ElementRef<HTMLElement>>(ElementRef);
   private cdr = inject(ChangeDetectorRef);
 
-  private readonly observerOptions = {
-    root: this.element.nativeElement,
-    rootMargin: this.rootMargin,
-    // Optional delay to reduce performance impact
-    // only works in Chromium browsers
-    ...{ delay: 100 },
-  } satisfies IntersectionObserverInit;
-
   private topSentinelElement = viewChild.required<ElementRef<HTMLElement>>('topSentinel');
   private bottomSentinelElement = viewChild.required<ElementRef<HTMLElement>>('bottomSentinel');
-
-  public pageSize = input(100);
-  public scrolledPage = toSignal(
-    fromEvent(this.element.nativeElement, 'scroll').pipe(
-      debounceTime(100),
-      map(() => Math.ceil(this.element.nativeElement.scrollTop / (this.pageSize() * this.itemHeight())))
-    ),
-    { initialValue: 1 }
-  );
-
-  protected page = linkedSignal(() => this.scrolledPage());
-
-  protected range = linkedSignal(() => {
-    const page = this.page();
-    const halfPageSize = this.pageSize() / 2;
-
-    const start = Math.max(0, (page - 1) * this.pageSize() - halfPageSize);
-    const end = Math.min(this.items().length, page * this.pageSize() + halfPageSize);
-
-    return { start, end };
-  });
-
-  public inRangeItems = computed(() => {
-    const { start, end } = this.range();
-    return this.items()
-      .slice(start, end)
-      .map((item, index) => ({ $index: start + index, item }));
-  });
-
-  protected intersections = new WeakMap<Element, boolean>();
-
-  /**
-   * @internal
-   * @private
-   */
-  public observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      this.intersections.set(entry.target, entry.isIntersecting);
-    });
-
-    this.cdr.markForCheck();
-  }, this.observerOptions);
 
   private itemHeight = computed(() => {
     return Math.max(
@@ -118,11 +81,62 @@ export class VirtualListComponent<T> {
     );
   });
 
+  public pageSize = input(100);
+  protected page = linkedSignal(
+    toSignal(
+      fromEvent(this.element.nativeElement, 'scroll').pipe(
+        debounceTime(100),
+        map(() => Math.ceil(this.element.nativeElement.scrollTop / (this.pageSize() * this.itemHeight())))
+      ),
+      { initialValue: 1 }
+    )
+  );
+
+  protected renderRange = linkedSignal(() => {
+    const page = this.page();
+    const halfPageSize = this.pageSize() / 2;
+
+    const start = Math.max(0, (page - 1) * this.pageSize() - halfPageSize);
+    const end = Math.min(this.items().length, page * this.pageSize() + halfPageSize);
+
+    return { start, end };
+  });
+
+  public renderedItems = computed(() => {
+    const { start, end } = this.renderRange();
+    return this.items()
+      .slice(start, end)
+      .map((item, index) => ({ $index: start + index, data: item }));
+  });
+
+  protected intersections = new WeakMap<Element, boolean>();
+
+  /**
+   * @internal
+   * @private
+   */
+  public observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        this.intersections.set(entry.target, entry.isIntersecting);
+      });
+
+      this.cdr.markForCheck();
+    },
+    {
+      root: this.element.nativeElement,
+      rootMargin: this.rootMargin,
+      // Optional delay to reduce performance impact
+      // only works in Chromium browsers
+      ...{ delay: 100 },
+    }
+  );
+
   constructor() {
     afterRenderEffect({
       write: () => {
-        this.topSentinelElement().nativeElement.style.height = `${this.range().start * this.itemHeight()}px`;
-        this.bottomSentinelElement().nativeElement.style.height = `${(this.items().length - this.range().end) * this.itemHeight()}px`;
+        this.topSentinelElement().nativeElement.style.height = `${this.renderRange().start * this.itemHeight()}px`;
+        this.bottomSentinelElement().nativeElement.style.height = `${(this.items().length - this.renderRange().end) * this.itemHeight()}px`;
       },
     });
 
@@ -161,7 +175,7 @@ export class VirtualListComponent<T> {
     runInInjectionContext(this.injector, () => {
       afterNextRender({
         earlyRead: () => {
-          const { start } = this.range();
+          const { start } = this.renderRange();
           const domIndex = index - start + 1; // Adjust for 1-based index
           return this.element.nativeElement.querySelector<HTMLElement>(
             `:nth-child(${domIndex + 1 /** Adjust for buffer element */})`
